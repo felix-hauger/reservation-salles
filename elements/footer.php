c9d89fe7a0329a5948d1ea2fbf6cf32b38d6b64f
felix-hauger <felix.hauger@laplateforme.io>
2022-12-14 12:16:44 +0100

    added header & footer elements with links

---
+++ elements/footer.php
@@ -0,0 +1,4 @@
+<footer>
+    <a href="https://felix-hauger.github.io/portfolio/">Portfolio</a>
+    <a href="https://www.linkedin.com/in/f%C3%A9lix-hauger/">LinkedIn</a>
+</footer>
---
+++ elements/header.php
@@ -0,0 +1,14 @@
+<header>
+    <h1>Réservation Salle</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Accueil</a></li>
+            <li><a href="signup.php">Inscription</a></li>
+            <li><a href="signin.php">Connexion</a></li>
+            <li><a href="personal-infos.php">Profil</a></li>
+            <li><a href="planning.php">Planning</a></li>
+            <li><a href="booking.php">Liste des Réservations</a></li>
+            <li><a href="booking-form.php">Réserver un Créneau</a></li>
+        </ul>
+    </nav>
+</header>
