a18e7802590c32d403e0fee9feb86a386a7293a4
felix-hauger <felix.hauger@laplateforme.io>
2022-12-14 17:43:53 +0100

    replaced function to check if all post inputs are filled by a class method in class/Form.php

---
+++ class/Form.php
@@ -0,0 +1,18 @@
+<?php
+
+class Form {
+
+
+    public static function are_all_posts_filled():bool {
+        $result = true;
+    
+        foreach ($_POST as $post) {
+            if ($post === '') {
+                $result = false;
+            }
+        }
+    
+        return $result;
+    }
+    
+}
