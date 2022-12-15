d40b770d23fa976e08bab0bfbd22f3e4fd2fef4d
felix-hauger <felix.hauger@laplateforme.io>
2022-12-15 15:18:12 +0100

    planning.php - separated processing & display codes & added comments

--- planning.php
+++ planning.php
@@ -10,6 +10,16 @@ $week_days = [
     'sunday'
 ];
 
+// will contains dates of the current week
+$week_dates = [];
+
+// convert week days to unix timestamp of the current week, format it then append it to the $week_dates array
+foreach ($week_days as $day) {
+    $day_ts = strtotime($day . ' this week');
+    $day_date = date('d/m/Y', $day_ts);
+    $week_dates[] = $day_date;
+}
+
 // $monday_ts = strtotime('monday this week');
 
 // $monday = date('d/m/Y', $monday_ts);
@@ -22,24 +32,14 @@ $week_days = [
 <table>
     <thead>
         <tr>
-            <?php
-
-            foreach ($week_days as $day) {
-                $day_ts = strtotime($day . ' this week');
-                $day_date = date('d/m/Y', $day_ts);
-                echo '<th>' . $day_date . '</th>';
-            }
-
-
-            ?>
+            <th></th>
+            <?php foreach ($week_dates as $date): ?>
+                <th><?= $date ?></th>
+            <?php endforeach ?>
         </tr>
     </thead>
     <?php
 
-    for ($i = 8; $i < 18; $i++) {
-        
-    }
-    
     // Hours
     for ($i = 8; $i < 18; $i++) {
         
