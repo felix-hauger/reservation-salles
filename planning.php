5fca40d8419f2b8dc19c40a5978f2eba5fc785f8
felix-hauger <felix.hauger@laplateforme.io>
2022-12-15 12:06:04 +0100

    added planning.php with current week monday in variables

---
+++ planning.php
@@ -0,0 +1,38 @@
+<?php
+
+$monday_ts = strtotime('monday this week');
+
+$monday = date('d/m/Y', $monday_ts);
+
+var_dump($monday);
+
+?>
+
+
+<table>
+    <thead>
+        <tr>
+            <?php
+
+            for ($i = 0; $i < 7; $i++) {
+
+            }
+
+
+            ?>
+        </tr>
+    </thead>
+    <?php
+
+    for ($i = 8; $i < 18; $i++) {
+        
+    }
+    
+    // Hours
+    for ($i = 8; $i < 18; $i++) {
+        
+    }
+
+
+    ?>
+</table>
