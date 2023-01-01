58af06d22fcebb30af582fb8105e551647d88fd7
felix-hauger <felix.hauger@laplateforme.io>
2023-01-01 21:18:41 +0100

    profile.php - update password & display errors from User->_errors array

--- profile.php
+++ profile.php
@@ -9,24 +9,46 @@ require_once 'class/User.php';
 
 // $pdo = $connect->pdo();
 
-require_once 'elements/dbconnect.php';
+require_once 'elements/dbconnect.php'; // require $pdo variable
 
+$errors = [];
 
 // $logged_user = new User($);
 
 if (isset($_POST['submit'])) {
+    // $password = '';
     var_dump($_POST);
-    if (isset($_POST['new-password'])) {
+    if (isset($_POST['new-password']) ) {
         $input_current_password = htmlspecialchars(trim($_POST['current-password']));
         $input_new_password = htmlspecialchars(trim($_POST['new-password']));
         $input_password_confirm = htmlspecialchars(trim($_POST['password-confirmation']));
 
-        // $logged_user = new User()
-        
-        if (Form::passConfirm($input_new_password, $input_password_confirm)) {
-            // $logged_user = new User($)
+        $logged_user = $_SESSION['logged_user_login'];
 
+        $update_user = new User($pdo, $logged_user, $input_current_password);
+
+        $current_pw_ok = $update_user->checkCredentials();
+
+        if (Form::areAllPostsFilled()) {
+            if (Form::passConfirm($input_new_password, $input_password_confirm)) {
+                $new_pw_ok = true;
+            } else {
+                $new_pw_ok = false;
+                $errors['passwords-differents'] = 'Champs des Mots de Passe différents.';
+            }
+    
+            var_dump($current_pw_ok, $new_pw_ok);
+    
+            var_dump($update_user->_errors);
+    
+            if ($update_user->checkCredentials() && $current_pw_ok) {
+                $options = ['cost' => 10];
+                $hashed_password = password_hash($input_new_password, PASSWORD_DEFAULT, $options);
+                $update_user->setPassword($hashed_password);
+                $update_user->updateInfo('password');
+            }
         }
+
     } else {
         
     }
@@ -41,6 +63,7 @@ if (isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profil | Réservation Salles</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <?php require_once 'elements/header.php' ?>
@@ -53,9 +76,18 @@ if (isset($_POST['submit'])) {
         </form>
         <form action="" method="post">
             <input type="password" name="current-password" id="current-password" placeholder="Mot de Passe actuel">
+            <?php if (isset($update_user->_errors['credentials'])): ?>
+                <p class="error"><?= $update_user->_errors['credentials'] ?></p>
+            <?php endif ?>
             <input type="password" name="new-password" id="new-password" placeholder="Nouveau Mot de Passe">
             <input type="password" name="password-confirmation" id="password-confirmation" placeholder="Confirmer Mot de Passe">
+            <?php if (isset(Form::$_errors['password'])): ?>
+                <p class="error"><?= Form::$_errors['password'] ?></p>
+            <?php endif ?>
             <input type="submit" name="submit" value="Modifier Mot de Passe">
+            <?php if (isset(Form::$_errors['unfilled'])): ?>
+                <p class="error"><?= Form::$_errors['unfilled'] ?></p>
+            <?php endif ?>
         </form>
     </main>
 
