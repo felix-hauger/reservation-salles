6f6514ae73343833f9540010d39b9316dfaea223
felix-hauger <felix.hauger@laplateforme.io>
2023-01-02 00:07:41 +0100

    profile.php - added comments & removed old commented code

--- profile.php
+++ profile.php
@@ -5,15 +5,10 @@ require_once 'class/DbConnection.php';
 require_once 'class/Form.php';
 require_once 'class/User.php';
 
-// $connect = new DbConnection('mysql', 'reservationsalles', 'localhost', 'root', '');
-
-// $pdo = $connect->pdo();
 
 require_once 'elements/dbconnect.php'; // require $pdo variable
 
 $errors = [];
-
-// $logged_user = new User($);
 
 if (isset($_POST['submit'])) {
     // $password = '';
@@ -26,8 +21,10 @@ if (isset($_POST['submit'])) {
 
         $update_user = new User($pdo, $_SESSION['logged_user_login'], $input_current_password);
 
+        // check entered password with db
         $current_pw_ok = $update_user->checkCredentials();
 
+        // and password fields separately to display errors in php
         if (Form::areAllPostsFilled()) {
             if (Form::passConfirm($input_new_password, $input_password_confirm)) {
                 $new_pw_ok = true;
@@ -43,19 +40,26 @@ if (isset($_POST['submit'])) {
             if ($update_user->checkCredentials() && $current_pw_ok) {
                 $options = ['cost' => 10];
                 $hashed_password = password_hash($input_new_password, PASSWORD_DEFAULT, $options);
+
+                // user methods to update instanciated object
                 $update_user->setPassword($hashed_password);
+
+                // then update pw in database
                 $update_user->updateInfo('password');
             }
         }
 
     } elseif (isset($_POST['login'])) {
         $input_login = htmlspecialchars(trim($_POST['login']));
+
+        // password not needed to update login
         $update_login = new User($pdo, $input_login, null);
 
         if (!$update_login->isLoginInDb()) {
-            // $update_login->setLogin($input_login);
             $update_login->updateInfo('login');
             $_SESSION['logged_user_login'] = $input_login;
+
+            // will be displayed in form
             $login_update_msg = 'Login modifié avec succès !';
             header('refresh: 3');
         } else {
