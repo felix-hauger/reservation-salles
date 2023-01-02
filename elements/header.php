179d5728fff0ca972e1753766389029330bb5719
felix-hauger <felix.hauger@laplateforme.io>
2023-01-02 01:55:26 +0100

    elements/header.php - display nav links depending if logged in or not

--- elements/header.php
+++ elements/header.php
@@ -3,13 +3,16 @@
     <nav>
         <ul>
             <li><a href="index.php">Accueil</a></li>
-            <li><a href="signup.php">Inscription</a></li>
-            <li><a href="signin.php">Connexion</a></li>
-            <li><a href="signout.php">Déconnexion</a></li>
-            <li><a href="profile.php">Profil</a></li>
             <li><a href="planning.php">Planning</a></li>
-            <li><a href="booking.php">Liste des Réservations</a></li>
-            <li><a href="booking-form.php">Réserver un Créneau</a></li>
+            <?php if (isset($_SESSION['is_logged'])): ?>
+                <li><a href="booking.php">Liste des Réservations</a></li>
+                <li><a href="booking-form.php">Réserver un Créneau</a></li>
+                <li><a href="profile.php">Profil</a></li>
+                <li><a href="signout.php">Déconnexion</a></li>
+            <?php else: ?>
+                <li><a href="signup.php">Inscription</a></li>
+                <li><a href="signin.php">Connexion</a></li>
+            <?php endif ?>
         </ul>
     </nav>
 </header>
