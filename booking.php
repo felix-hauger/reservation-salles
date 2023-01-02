7cc2f062d1dcc18bf7cb656cd440ab37d3cdcf15
felix-hauger <felix.hauger@laplateforme.io>
2023-01-02 01:23:27 +0100

    booking.php - added html structure

--- booking.php
+++ booking.php
@@ -48,3 +48,21 @@ if (!$booking = get_booking()) {
 
 var_dump($booking);
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php require_once 'elements/header.php' ?>
+
+    <main>
+
+    </main>
+
+    <?php require_once 'elements/footer.php' ?>
+</body>
+</html>
