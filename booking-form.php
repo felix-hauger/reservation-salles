0ac3b7bfe5ac3a89bb78ab51e16d1dc31d824721
felix-hauger <felix.hauger@laplateforme.io>
2023-01-02 03:30:04 +0100

    booking-form.php - get bookings infos with post form & create start & end with DateTime

--- booking-form.php
+++ booking-form.php
@@ -13,6 +13,27 @@ var_dump($min_date);
 // $min_date = $min_date . ':00';
 // var_dump($min_date);
 
+function create_booking($title, $description, $start, $end) {
+    require_once 'class/DbConnection.php';
+    require_once 'elements/dbconnect.php';
+
+    // $sql = 'SELECT start, end FROM bookings';
+
+    // if ()
+
+    $sql = 'INSERT INTO bookings (title, description, start, end) VALUES (:title, :description, :start, :end)';
+
+    $insert = $pdo->prepare($sql);
+
+    $insert->bindParam(':title', $title);
+    $insert->bindParam(':description', $description);
+    $insert->bindParam(':start', $start);
+    $insert->bindParam(':end', $end);
+
+    $insert->execute();
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -25,8 +46,6 @@ var_dump($min_date);
 </head>
 <body>
     <?php require_once 'elements/header.php'; ?>
-
-
 
     <h2>Formulaire de réservation</h2>
     <form action="" method="post">
@@ -42,8 +61,8 @@ var_dump($min_date);
             <?php endfor ?>
         </select>
 
-        <label for="end">Heure de fin</label>
-        <select name="end" id="end">
+        <label for="end-hour">Heure de fin</label>
+        <select name="end-hour" id="end-hour">
             <?php for ($i = 8; $i <= 18; $i++): ?>
                 <option value="<?= $i ?>"><?= $i . 'h' ?></option>
             <?php endfor ?>
@@ -52,13 +71,8 @@ var_dump($min_date);
         <label for="description">Description</label>
         <textarea name="description" id="description" cols="30" rows="10" placeholder="description..."></textarea>
 
-        <input type="submit" value="Réserver">
+        <input type="submit" name="submit" value="Réserver">
     </form>
-
-
-
-
-
 
     <?php require_once 'elements/footer.php'; ?>
 </body>
