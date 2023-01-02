07eba61c90c6389722afd8d83ef6ba8217ae5ce0
felix-hauger <felix.hauger@laplateforme.io>
2023-01-02 01:24:09 +0100

    booking.php - added booking title in html title tag

--- booking.php
+++ booking.php
@@ -55,6 +55,7 @@ var_dump($booking);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $booking['title'] ?> | Réservation Salle</title>
 </head>
 <body>
     <?php require_once 'elements/header.php' ?>
